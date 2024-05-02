2d3b467d45358bc67d5b3d02aea20c60594e5247
gabrielnav <gabrielnav.j@gmail.com>
2024-05-02 20:55:50 -0300

    Instalar Chackra UI

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+    
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
@@ -28,6 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
+   
   )
 }
 
